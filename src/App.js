74dad22520467fb53a73a5752c28524f9cf3546e
Sarah Sipple <hello@sarahsipple.com>
2021-03-29 16:54:27 -0400

    added react router dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
 import './App.css';
 import HomePage from './pages/homepage/homepage';
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route path='/' component={HomePage} />
+      </Switch>
     </div>
   );
 }
