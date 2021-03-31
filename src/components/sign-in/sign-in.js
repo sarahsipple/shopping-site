a8a09d62236b6e242def6c12fd5115aba888aa9c
Sarah Sipple <hello@sarahsipple.com>
2021-03-31 12:21:53 -0400

    added signin component and button

---
+++ src/components/sign-in/sign-in.js
@@ -0,0 +1,58 @@
+import React from 'react';
+
+import FormInput from '../form-input/form-input';
+import CustomButton from '../custom-button/custom-button';
+
+import './sign-in.styles.scss';
+
+class SignIn extends React.Component{
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            email: '',
+            password: ''
+        }
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+
+        this.setState({ email:'', password: '' })
+    }
+
+    handleChange = (e) => {
+        const {value, name} = e.target;
+        this.setState({ [name]: value })
+    }
+
+    render() {
+        return(
+            <div className="sign-in">
+                <h2>I already have an account</h2>
+                <span>Sign In With Your Email And Password</span>
+                <form onSubmit={this.handleSubmit}>
+                    <FormInput 
+                        name="email" 
+                        type="email" 
+                        label="EMAIL"
+                        handleChange={this.handleChange}
+                        value={this.state.email} 
+                        required 
+                    />
+                    <FormInput 
+                        name="password" 
+                        type="password" 
+                        label="PASSWORD"
+                        handleChange={this.handleChange}
+                        value={this.state.password} 
+                        required 
+                    />
+                    <CustomButton type="submit">Sign In</CustomButton>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default SignIn;
---
+++ src/pages/sign-in-register/sign-in-register.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import SignIn from '../../components/sign-in/sign-in';
+
+import './sign-in-register.styles.scss';
+
+const SignInAndRegisterPage = () => (
+    <div className="sign-in-and-sign-up">
+        <SignIn />
+    </div>
+)
+
+export default SignInAndRegisterPage;
