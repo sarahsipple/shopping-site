d3d03ad2c058c9f517ecb3ca201eb7ef8502fd76
Sarah Sipple <hello@sarahsipple.com>
2021-04-08 11:00:36 -0400

    fixed error on cart dropdown

--- src/components/cart-dropdown/cart-dropdown.js
+++ src/components/cart-dropdown/cart-dropdown.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 
 import CustomButton from '../custom-button/custom-button';
 import CartItem from '../cart-item/cart-item';
@@ -7,19 +7,18 @@ import CartItem from '../cart-item/cart-item';
 import './cart-dropdown.styles.scss';
 
 const CartDropdown = ({ cartItems }) => (
-    <div className="cart-dropdown">
-        <div className="cart-items">
-            {
-                cartItems.map(cartItem => <CartItem key={cartItem.id} item={CartItem} />)
-            }
-        </div>
-
-        <CustomButton>GO TO CHECKOUT</CustomButton>
+  <div className='cart-dropdown'>
+    <div className='cart-items'>
+      {cartItems.map(cartItem => (
+        <CartItem key={cartItem.id} item={cartItem} />
+      ))}
     </div>
-)
+    <CustomButton>GO TO CHECKOUT</CustomButton>
+  </div>
+);
 
-const mapStateToProps = ({ cart: { cartItems }}) => ({
-    cartItems
-})
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+  cartItems
+});
 
 export default connect(mapStateToProps)(CartDropdown);
