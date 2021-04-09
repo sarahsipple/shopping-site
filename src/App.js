e19ffd9b36e42e7001b049a616d5bf58a3a9e7bb
Sarah Sipple <hello@sarahsipple.com>
2021-04-09 13:39:57 -0400

    added checkoutpage and router from cartdropdown

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import HomePage from './pages/homepage/homepage';
 import ShopPage from './pages/shop/shop';
 import SignInAndRegisterPage from './pages/sign-in-register/sign-in-register';
 import Header from './components/header/header';
+import CheckoutPage from './pages/checkout/checkout';
 
 import { auth, createUserProfileDocument } from './firebase/firebase';
 
@@ -48,6 +49,7 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
+          <Route exact path='/checkout' component={CheckoutPage} />
           <Route exact path='/signin' render={() => this.props.currentUser ? (<Redirect to='/' />) : (<SignInAndRegisterPage />)} />
         </Switch>
       </div>
