bc653e7e37bfb816969e8ffd56a68bebbf46a297
Sarah Sipple <hello@sarahsipple.com>
2021-04-15 09:43:46 -0400

    updated favicon and fixed bug on shop selector

--- src/components/collections-overview/collections-overview.js
+++ src/components/collections-overview/collections-overview.js
@@ -1,21 +1,23 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
-import CollectionPreview from '../../components/collection-preview/collection-preview';
-import { selectCollections } from '../../redux/shop/shop.selectors';
+
+import CollectionPreview from '../collection-preview/collection-preview';
+
+import { selectCollectionsForPreview } from '../../redux/shop/shop.selectors';
 
 import './collections-overview.styles.scss';
 
 const CollectionsOverview = ({ collections }) => (
-    <div className="collections-overview">
+  <div className='collections-overview'>
     {collections.map(({ id, ...otherCollectionProps }) => (
-            <CollectionPreview key={id} {...otherCollectionProps} />
+      <CollectionPreview key={id} {...otherCollectionProps} />
     ))}
-</div>
+  </div>
 );
 
 const mapStateToProps = createStructuredSelector({
-    collections: selectCollections
+  collections: selectCollectionsForPreview
 });
 
 export default connect(mapStateToProps)(CollectionsOverview);
